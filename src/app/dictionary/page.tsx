5be7f5f7093a06f1aff8893c67bd1d95ca2e33c2
Cruizzer <archcruizer@gmail.com>
2023-07-15 18:56:03 +0100

    fixed key props

--- src/app/dictionary/page.tsx
+++ src/app/dictionary/page.tsx
@@ -49,6 +49,8 @@ const Dictionary = () => {
     return null
   }
 
+  console.log(data)
+
 
   return (
     <>
@@ -59,8 +61,8 @@ const Dictionary = () => {
 
       {/* Translations */}
       <div className="grid grid-cols-3 gap-4 p-10">
-        {data.map((translation: Translation) => (
-          <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        {data.map((translation: Translation, i: number) => (
+          <a key={i} href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{translation.ENGLISH}</h5>
             <p className="font-normal text-gray-700 dark:text-gray-400">{translation.KONKANI}</p>
           </a>
