3d795d27ec23c5535719ae57745b1379bcb0957c
Cruizzer <archcruizer@gmail.com>
2023-08-08 14:39:38 +0100

    Merged current styling to match shadcn theming

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import AuthProvider from '@/components/AuthProvider'
+import { cn } from '@/lib/utils'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,8 +18,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className="min-h-screen antialiased">
+    <html lang="en" className={cn('bg-black text-slate-900 antialiased light', inter.className)}>
+      <body className="bg-black text-white min-h-screen antialiased">
         <AuthProvider>
           <div>
             <Navbar />
